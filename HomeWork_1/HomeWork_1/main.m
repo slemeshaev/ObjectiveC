3c597608774b887775426d1fee0c6da736593c74
slemeshaev <lemeshaevs@gmail.com>
2021-03-21 23:20:56 +0300

    done

--- HomeWork_1/HomeWork_1/main.m
+++ HomeWork_1/HomeWork_1/main.m
@@ -9,8 +9,26 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // 1. Создать программу, которая будет пременять к введенным числам различные арифметические операции (на основе практической задачи 3).
+        double firstNumber;
+        double secondNumber;
+        NSLog(@"\nFirst number: ");
+        scanf("%lf", &firstNumber);
+        NSLog(@"\nSecond number: ");
+        scanf("%lf", &secondNumber);
+        NSLog(@"\nSum result = %f", firstNumber + secondNumber);
+        NSLog(@"\nDiff result = %f", firstNumber - secondNumber);
+        NSLog(@"\nMult result = %f", firstNumber * secondNumber);
+        NSLog(@"\nDiv result = %f", firstNumber / secondNumber);
+        
+        // 2. Улучшить программу: организовать вывод результата и переменных в консоль одной строкой (как в практической задаче 2).
+        NSLog(@"\nSum result = %f, Diff result = %f, Mult result = %f, Div result = %f", firstNumber + secondNumber, firstNumber - secondNumber, firstNumber * secondNumber, firstNumber / secondNumber);
+        
+        // 3. *Создать приложение, которое будет вычислять среднее число из трех переменных, не применяя специальные функции
+        double thirdNumber;
+        NSLog(@"\nThird number: ");
+        scanf("%lf", &thirdNumber);
+        NSLog(@"\nAverage number = %f", (firstNumber + secondNumber + thirdNumber) / 3);
     }
     return 0;
 }
