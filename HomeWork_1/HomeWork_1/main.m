923efbc4842fa7a72d31fc6f6e072b160127a409
slemeshaev <lemeshaevs@gmail.com>
2021-03-21 23:20:49 +0300

    home work 1 is done

---
+++ HomeWork_1/HomeWork_1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HomeWork_1
+//
+//  Created by Станислав Лемешаев on 21.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
