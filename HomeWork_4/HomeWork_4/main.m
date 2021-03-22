d807ee42457d73f58bb324c7396777530eba1b5c
slemeshaev <lemeshaevs@gmail.com>
2021-03-22 19:30:27 +0300

    project files

---
+++ HomeWork_4/HomeWork_4/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HomeWork_4
+//
+//  Created by Станислав Лемешаев on 22.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
