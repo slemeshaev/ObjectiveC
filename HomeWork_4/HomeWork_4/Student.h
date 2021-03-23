d204ea12d8138fe646f964956d0b87be7514d433
slemeshaev <lemeshaevs@gmail.com>
2021-03-23 17:38:22 +0300

    student model

---
+++ HomeWork_4/HomeWork_4/Student.h
@@ -0,0 +1,16 @@
+//
+//  Student.h
+//  HomeWork_4
+//
+//  Created by Станислав Лемешаев on 23.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Student : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HomeWork_4/HomeWork_4/Student.m
@@ -0,0 +1,12 @@
+//
+//  Student.m
+//  HomeWork_4
+//
+//  Created by Станислав Лемешаев on 23.03.2021.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
