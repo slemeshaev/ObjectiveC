17eec199b4c8e95819373c2157e362aa98a726e6
Stanislav Lemeshaev <s.lemeshaev@haulmont.com>
2021-03-28 20:30:08 +0300

    model car

--- ProgramCar/ProgramCar/Model/Car.h
+++ ProgramCar/ProgramCar/Model/Car.h
@@ -6,10 +6,16 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Engine.h"
+#import "Wheel.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface Car : NSObject
+
+- (void)configWithEngine:(Engine *)engine andWheels:(NSArray *)wheels;
+@property (nonatomic, strong) Engine *engine;
+@property (nonatomic, strong) NSArray *wheels; 
 
 @end
 
--- ProgramCar/ProgramCar/Model/Car.m
+++ ProgramCar/ProgramCar/Model/Car.m
@@ -9,4 +9,46 @@
 
 @implementation Car
 
+- (instancetype) init
+{
+    self = [super init];
+    if (self) {
+        NSLog(@"Create car");
+    }
+    return self;
+}
+
+- (void)configWithEngine:(Engine *)engine andWheels:(NSArray *)wheels
+{
+    [engine retain];
+    [engine release];
+    self.engine = engine;
+    NSLog(@"Add engine for car");
+    
+    [wheels retain];
+    [wheels release];
+    self.wheels = wheels;
+    for (Wheel *wheel in wheels)
+    {
+        NSLog(@"Add wheel (%@) for car", wheel.number);
+    }
+}
+
+- (void)remove
+{
+    NSLog(@"Remove engine and wheels from car");
+    for (Wheel *wheel in self.wheels)
+    {
+        [wheel release];
+    }
+    [self.wheels release];
+    [self.engine release]; 
+}
+
+- (void)dealloc
+{
+    [self remove];
+    NSLog(@"Dealloc car");
+    [super dealloc]; 
+}
 @end
